af92e513908e32534c76fd02185ae5edb8634edb
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-04-10 13:38:19 +0300

    Add forms.

---
+++ ProjectSoftUni/ProjectSoftUni/accounts_app/forms.py
@@ -0,0 +1,65 @@
+import datetime
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
+
+from ProjectSoftUni.accounts_app.models import Profile
+
+UserModel = get_user_model()
+
+
+class UserRegistrationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs['autofocus'] = False
+
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+
+        self.fields['password2'].help_text = None
+        self.fields['password1'].help_text = None
+
+        self.fields['email'].label = ''
+        self.fields['password1'].label = ''
+        self.fields['password2'].label = ''
+
+    class Meta:
+        model = UserModel
+        fields = ('email', 'password1', 'password2')
+
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['autofocus'] = False
+
+        self.fields['username'].widget.attrs['placeholder'] = 'Email'
+        self.fields['password'].widget.attrs['placeholder'] = 'Password'
+
+        self.fields['username'].label = ''
+        self.fields['password'].label = ''
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user', )
+        widgets = {
+            'picture': forms.FileInput(),
+            'date_of_birth': forms.DateInput(
+                attrs={
+                    'type': 'date',
+                    'min': '1920-01-01',
+                    'max': datetime.date.today()
+                }
+            ),
+        }
+        labels = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'date_of_birth': 'Date Of Birth',
+        }
