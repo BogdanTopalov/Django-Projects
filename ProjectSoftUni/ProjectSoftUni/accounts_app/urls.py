196bcdd347b1f393482c618117b53205d99cd0c3
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-04-11 23:30:15 +0300

    Update accpunts_app urls.

--- ProjectSoftUni/ProjectSoftUni/accounts_app/urls.py
+++ ProjectSoftUni/ProjectSoftUni/accounts_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from ProjectSoftUni.accounts_app.views import ShowProfileView, RegisterProfileView, DeleteProfileView, LoginProfileView, LogoutProfileView, UpdateProfileView
+from ProjectSoftUni.accounts_app.views import ShowProfileView, RegisterProfileView, DeleteProfileView,\
+    LoginProfileView, LogoutProfileView, UpdateProfileView, ProfileRecipesView
 
 urlpatterns = [
     path('<int:pk>/', ShowProfileView.as_view(), name='show profile page'),
@@ -8,7 +9,8 @@ urlpatterns = [
     path('login/', LoginProfileView.as_view(), name='login profile page'),
     path('logout/', LogoutProfileView.as_view(), name='logout profile'),
     path('<int:pk>/delete/', DeleteProfileView.as_view(), name='delete profile page'),
-    path('<int:pk>/details/', UpdateProfileView.as_view(), name='update profile page'),
+    path('<int:pk>/edit/', UpdateProfileView.as_view(), name='edit profile page'),
+    path('<int:pk>/recipes/', ProfileRecipesView.as_view(), name='profile recipes page'),
 
 ]
 
