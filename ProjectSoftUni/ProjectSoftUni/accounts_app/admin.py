2e1a87a2992bc934711ba3bd6c6ee444dccdcae0
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-04-13 18:43:27 +0300

    Add custom admin.

--- ProjectSoftUni/ProjectSoftUni/accounts_app/admin.py
+++ ProjectSoftUni/ProjectSoftUni/accounts_app/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, AdminPasswordChangeForm
+from django.utils.translation import gettext_lazy as _
+from ProjectSoftUni.accounts_app.forms import UserRegistrationForm
+from ProjectSoftUni.accounts_app.models import AccountsCustomUser, Profile
+
+
+@admin.register(AccountsCustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form_template = "admin/auth/user/add_form.html"
+    change_user_password_template = None
+    fieldsets = (
+        (None, {"fields": ()}),
+        (_("Personal info"), {"fields": ("email",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    form = UserChangeForm
+    add_form = UserRegistrationForm
+    change_password_form = AdminPasswordChangeForm
+    list_display = ("email", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("email",)
+    ordering = ("email",)
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+
+
+@admin.register(Profile)
+class ProfilesAdmin(admin.ModelAdmin):
+    pass
+
