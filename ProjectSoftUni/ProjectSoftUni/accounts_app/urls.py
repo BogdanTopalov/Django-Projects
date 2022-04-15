3880af1d06bc6f7df9d4991af89d155625135994
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-04-15 23:20:42 +0300

    Update accounts_app urls.

--- ProjectSoftUni/ProjectSoftUni/accounts_app/urls.py
+++ ProjectSoftUni/ProjectSoftUni/accounts_app/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 
-from ProjectSoftUni.accounts_app.views import ShowProfileView, RegisterProfileView, DeleteProfileView,\
-    LoginProfileView, LogoutProfileView, UpdateProfileView, ProfileRecipesView
+from ProjectSoftUni.accounts_app.views.user import RegisterUserView, LoginUserView, LogoutUserView
+
+from ProjectSoftUni.accounts_app.views.profile import \
+    ShowProfileView, DeleteProfileView, UpdateProfileView, ProfileRecipesView
 
 urlpatterns = [
+    path('register/', RegisterUserView.as_view(), name='register user page'),
+    path('login/', LoginUserView.as_view(), name='login user page'),
+    path('logout/', LogoutUserView.as_view(), name='logout user'),
+
     path('<int:pk>/', ShowProfileView.as_view(), name='show profile page'),
-    path('register/', RegisterProfileView.as_view(), name='register profile page'),
-    path('login/', LoginProfileView.as_view(), name='login profile page'),
-    path('logout/', LogoutProfileView.as_view(), name='logout profile'),
     path('<int:pk>/delete/', DeleteProfileView.as_view(), name='delete profile page'),
     path('<int:pk>/edit/', UpdateProfileView.as_view(), name='edit profile page'),
     path('<int:pk>/recipes/', ProfileRecipesView.as_view(), name='profile recipes page'),
-
 ]
 
 import ProjectSoftUni.accounts_app.signals
