1dd5b6e3f0e6d79c2f2df2b59561e02e588705b8
BogdanTopalov <89787623+BogdanTopalov@users.noreply.github.com>
2022-04-15 23:39:07 +0300

    Add main_app admin classes.

--- ProjectSoftUni/ProjectSoftUni/main_app/admin.py
+++ ProjectSoftUni/ProjectSoftUni/main_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from ProjectSoftUni.main_app.models import Recipe, Product, Course
+
+
+@admin.register(Recipe)
+class RecipesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Course)
+class CoursesAdmin(admin.ModelAdmin):
+    pass
